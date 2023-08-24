94a53d6f50ade36f745ab8e05db9c17d3bfbb6e8
blloop <42186044+blloop@users.noreply.github.com>
2023-08-23 20:19:19 -0800

    add basic hover animation

--- js/script.js
+++ js/script.js
@@ -12,8 +12,50 @@ const box = new Sprite({
   source: './img/textbox.png'
 })
 
+let choice1 = [550, 330, 340, 60, false];
+let choice2 = [550, 410, 340, 60, false];
+function drawBox(box) {
+  c.fillStyle = box[4] ? 'yellow' : 'blue'
+  c.fillRect(box[0], box[1], box[2], box[3])
+}
+
 function loop() {
   box.update();
+  drawBox(choice1);
+  drawBox(choice2);
   window.requestAnimationFrame(loop);
 }
 loop();
+
+function cursorOn(x, y, box) {
+  return (x > box[0] && y > box[1] && 
+    x < box[0] + box[2] && y < box[1] + box[3]
+  );
+}
+window.onmousemove = function(e) {
+  let rect = canvas.getBoundingClientRect();
+  let mX = e.pageX - rect.left;
+  let mY = e.pageY - rect.top;
+  if (cursorOn(mX, mY, choice1)) {
+    canvas.style.cursor = 'pointer';
+    choice1[4] = true;
+  } else if (cursorOn(mX, mY, choice2)) {
+    canvas.style.cursor = 'pointer';
+    choice2[4] = true;
+  } else {
+    canvas.style.cursor = 'default';
+    choice1[4] = false;
+    choice2[4] = false;
+  }
+}
+
+window.onmouseup = function(e) {
+  let rect = canvas.getBoundingClientRect();
+  let mX = e.pageX - rect.left;
+  let mY = e.pageY - rect.top;
+  if (cursorOn(mX, mY, choice1)) {
+    return;
+  } else if (cursorOn(mX, mY, choice2)) {
+    return;
+  }
+}
