7a8ca9be4e7329d4c09e007137b73a1d9799ecaf
blloop <42186044+blloop@users.noreply.github.com>
2023-08-24 20:19:19 -0800

    add constant refactoring, more choice support

--- js/script.js
+++ js/script.js
@@ -6,21 +6,41 @@ const c = canvas.getContext('2d');
 c.imageSmoothingEnabled = false;
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+// Game constants
+const NUMCHOICE = 3; // number of user choices
+const INDWIDTH = 20; // indicator width, in px
+const CBOXW = 400; // width of choice box, in px
+const CBOXH = 200; // height of choice box, in px
+const CBOXPAD = 40; // padding of choice box, in px
+const PAGEPAD = 40; // dialog page padding, in px
+const TSDELAY = 400; // transition delay, in ms
+
+// Interface elements
 const box = new Sprite({
   position: { x: 520, y: 300 },
-  size: { width: 400, height: 200 },
+  size: { width: CBOXW, height: CBOXH },
   source: './img/textbox.png'
 })
 
-let choice1 = ['Go Left', 590, 370, 340, false];
-let choice2 = ['Go Right', 590, 440, 340, false];
-let box1 = [550, 330, 340, 60];
-let box2 = [550, 410, 340, 60];
+let choices = [];
+for (let i = 0; i < NUMCHOICE; i++) {
+  choices.push([
+    'Blank Choice', 
+    canvas.width - PAGEPAD - CBOXW + CBOXPAD + INDWIDTH, 
+    360 + (i * 50), 
+    false
+  ]);
+}
 
+let box1 = [550, 330, 340, 40];
+let box2 = [550, 380, 340, 40];
+let box3 = [550, 430, 340, 40];
+
+console.log(choices);
+// Event loop
 function loop() {
   box.update();
-  drawChoice(choice1);
-  drawChoice(choice2);
+  choices.forEach(ch => drawChoice(ch));
   drawScene(0);
   window.requestAnimationFrame(loop);
 }
@@ -36,19 +56,14 @@ window.onmousemove = function(e) {
   let rect = canvas.getBoundingClientRect();
   let mX = e.pageX - rect.left;
   let mY = e.pageY - rect.top;
-  if (cursorOn(mX, mY, box1)) {
-    canvas.style.cursor = 'pointer';
-    choice1[4] = true;
-    choice2[4] = false;
-  } else if (cursorOn(mX, mY, box2)) {
-    canvas.style.cursor = 'pointer';
-    choice1[4] = false;
-    choice2[4] = true;
-  } else {
-    canvas.style.cursor = 'default';
-    choice1[4] = false;
-    choice2[4] = false;
-  }
+  choices[0][3] = cursorOn(mX, mY, box1);
+  choices[1][3] = (!choices[0][3] && 
+    cursorOn(mX, mY, box2));
+  choices[2][3] = (!choices[0][3] && 
+    !choices[1][3] && cursorOn(mX, mY, box3));
+  canvas.style.cursor = 
+    (choices[0][3] || choices[1][3] || choices[2][3]) ?
+    'pointer' : 'default';
 }
 
 window.onmouseup = function(e) {
--- js/temp.js
+++ js/temp.js
@@ -1,10 +1,8 @@
 function drawChoice(x) {
-  c.fillStyle = x[4] ? 'pink' : 'white';
+  c.fillStyle = x[3] ? 'pink' : 'white';
   c.font = '24px Verdana';
-  c.fillText(x[0], x[1], x[2], x[3]);
-  if (x[4]) c.fillText('>', x[1] - 40, x[2], x[3])
-  // c.fillStyle = x[4] ? 'yellow' : 'blue'
-  // c.fillRect(x[0], x[1], x[2], x[3]);
+  c.fillText(x[0], x[1], x[2], CBOXW - (2 * CBOXPAD));
+  if (x[3]) c.fillText('>', x[1] - INDWIDTH, x[2], INDWIDTH)
 }
 
 function drawScene(num) {
